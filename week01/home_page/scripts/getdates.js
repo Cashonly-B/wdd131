f95fcc93cc01b3da83b4355d1a76602af25f2553
Cashonly-B <bon23011@byui.edu>
2024-04-26 16:39:47 -0600

    Added code to html and added new js script w folder

---
+++ week01/home_page/scripts/getdates.js
@@ -0,0 +1,11 @@
+/* Get the last date and time modified in JavaScript */
+/* In this format 10/10/1001 10:10:10 */
+function getLastModifiedDateTime() {
+    var lastModified = new Date(document.lastModified);
+    var formattedDate = ('0'+ (lastModified.getMonth() + 1)).slice(-2) + '/' + ('0' + lastModified.getDate()).slice(-2) + '/' + lastModified.getFullYear();
+    var formattedTime = ('0'+ lastModified.getHours()).slice(-2) + ':' + ('0' + lastModified.getMinutes()).slice(-2) + ':' + ('0' + lastModified.getSeconds()).slice(-2); 
+    return formattedDate + ' ' + formattedTime;
+    }
+    
+    /* Get the result from the function and put in back in the footer */
+    document.getElementById('lastmodified').textContent = getLastModifiedDateTime();
