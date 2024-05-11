62b461ae8f7e1af2048a10c90eae94a931e70e30
Cashonly-B <bon23011@byui.edu>
2024-05-10 18:25:03 -0600

    Added windChill to javascript

--- scripts/place.js
+++ scripts/place.js
@@ -9,3 +9,28 @@ function getLastModifiedDateTime() {
     
     /* Get the result from the function and put in back in the footer */
     document.getElementById('lastmodified').textContent = getLastModifiedDateTime();
+
+
+
+function calculateWindChill(temperature, windSpeed) {
+    /* Formula for calculating the windchill */
+    return (temperature <= 50 && windSpeed > 3) ?
+        Math.round(13.12 + 0.6215 * temperature - 11.37 * Math.pow(windSpeed, 0.16) + 0.3965 * temperature * Math.pow(windSpeed, 0.16)) :
+        "N/A";
+}
+
+/* Display the wind chill */
+function displayWindChill() {
+    const temperatureElement = document.querySelector('.weather li:nth-child(1)');
+    const windSpeedElement = document.querySelector('.weather li:nth-child(3)');
+    const windChillElement = document.querySelector('.weather li:nth-child(4)');
+
+    const temperatureValue = parseFloat(temperatureElement.textContent.match(/\d+/)[0]);
+    const windSpeedValue = parseFloat(windSpeedElement.textContent.match(/\d+/)[0]);
+
+    const windChill = calculateWindChill(temperatureValue, windSpeedValue);
+    /* Display windchill factor in Fahrenheit */
+    windChillElement.textContent = `Wind Chill: ${windChill}°F`; 
+}
+
+window.addEventListener('load', displayWindChill);
