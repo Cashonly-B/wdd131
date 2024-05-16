7b3faace639a70834ca37545ea4416b40337e6ec
Cashonly-B <bon23011@byui.edu>
2024-05-16 10:53:42 -0600

    Copied and made filtered temple files

---
+++ scripts/filtered-temples.js
@@ -0,0 +1,20 @@
+/* Get the last date and time modified in JavaScript */
+/* In this format 10/10/1001 10:10:10 */
+function getLastModifiedDateTime() {
+    var lastModified = new Date(document.lastModified);
+    var formattedDate = ('0'+ (lastModified.getMonth() + 1)).slice(-2) + '/' + ('0' + lastModified.getDate()).slice(-2) + '/' + lastModified.getFullYear();
+    var formattedTime = ('0'+ lastModified.getHours()).slice(-2) + ':' + ('0' + lastModified.getMinutes()).slice(-2) + ':' + ('0' + lastModified.getSeconds()).slice(-2); 
+    return formattedDate + ' ' + formattedTime;
+    }
+    
+/* Get the result from the function and put in back in the footer */
+document.getElementById('lastmodified').textContent = getLastModifiedDateTime();
+
+
+const hamButton = document.querySelector('#menu');
+const navigation = document.querySelector('.navigation');
+
+hamButton.addEventListener('click', () => {
+	navigation.classList.toggle('open');
+	hamButton.classList.toggle('open');
+});
